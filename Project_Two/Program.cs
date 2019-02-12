e8f4eae311bf02469ff1ab3284635ff76b829b47
Egle <eglandj@dunwoody.edu>
2019-02-12 14:37:37 -0600

    Writing to a file

--- Project_Two/Program.cs
+++ Project_Two/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace Project_Two
 {
@@ -16,7 +17,7 @@ namespace Project_Two
                                            .Skip(1)
                                            .Select(v => SuperBowl.FromCsv(v))
                                            .ToList();
-            TextFile();
+            TextFile(information);
             /*foreach (SuperBowl bowl in information)
             {
                 Console.WriteLine(bowl.Date.Year);
@@ -25,32 +26,49 @@ namespace Project_Two
             }
             Console.ReadKey();*/
         }
-        public static void CenterConsoleWrite(int length, string text)
-        {
-            int stringLength = text.Length;
-            int buffer = (length - stringLength) / 2;
-            string spacer = "";
-            for (int i = 0; i < buffer; i++)
-            {
-                spacer += " ";
-            }
-            if (stringLength % 2 == 0)
-            {
-                Console.Write(spacer + text + spacer);
-            }
-            else
-            {
-                Console.Write(" " + spacer + text + spacer);
-            }
-        }
-        public static void TextFile()
+        public static void TextFile(List<SuperBowl> information)
         {
             string filePath = Path.Combine(
                 Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
                 "SuperBowl.txt");
-            Console.WriteLine(filePath);
+            /*Console.WriteLine(filePath);
             TextWriter sw = new StreamWriter(filePath);
-            Console.ReadKey();
+            Console.ReadKey();*/
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+
+            //Create the file.
+            using (FileStream fs = File.Create(filePath))
+            {
+                for (int i = 0; i < information.Count; i++)
+                {
+                    AddText(fs, information[i].SuperBowlNumber.PadRight(20));
+                    AddText(fs, Convert.ToString(information[i].Date.Year).PadRight(20));
+                    AddText(fs, "\r\n");
+                    /*AddText(fs, "This is some more text,");
+                    AddText(fs, "\r\nand this is on a new line");
+                    AddText(fs, "\r\n\r\nThe following is a subset of characters:\r\n");*/
+                }
+            }
+
+            //Open the stream and read it back.
+            using (FileStream fs = File.OpenRead(filePath))
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    Console.WriteLine(temp.GetString(b));
+                }
+            }
+        }
+
+        private static void AddText(FileStream fs, string value)
+        {
+            byte[] info = new UTF8Encoding(true).GetBytes(value);
+            fs.Write(info, 0, info.Length);
         }
     }
 }
